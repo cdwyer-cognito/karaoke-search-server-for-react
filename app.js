15fed92c1f59ac6770c4a0f33b6fcb44079c7c84
cdwyer-cognito <chris.dwyer@cognitoiq.com>
2019-01-27 16:51:13 +0000

    added 404 path to route back to app home

--- app.js
+++ app.js
@@ -42,7 +42,8 @@ app.get('/', function(req, res) {
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  // next(createError(404));
 });
 
 // error handler
