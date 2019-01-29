99b796849858da1f88d5735c6f192d8fe461e7fd
cdwyer-cognito <chris.dwyer@cognitoiq.com>
2019-01-29 21:49:57 +0000

    Added console output for network data

---
+++ src/ipConfigData.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const os = require('os');
+
+function ipConfigData() {
+    const ifaces = os.networkInterfaces();
+    const addresses = [{ name: 'DNS', alias: '', address: os.hostname() }];
+    Object.keys(ifaces).forEach(function (ifname) {
+    let alias = 0;
+
+        ifaces[ifname].forEach(function (iface) {
+            if ('IPv4' !== iface.family || iface.internal !== false) {
+            // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+            return;
+            }
+
+            if (alias >= 1) {
+            // this single interface has multiple ipv4 addresses
+                addresses.push({ name: ifname, alias: alias, address: iface.address });
+            } else {
+            // this interface has only one ipv4 adress
+                addresses.push({ name: ifname, alias: "", address: iface.address });
+            }
+            ++alias;
+        });
+    });
+
+    return addresses;
+}
+
+module.exports = ipConfigData;
