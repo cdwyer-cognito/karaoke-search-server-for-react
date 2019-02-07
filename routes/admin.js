fde193db8fc5e81a5302b07215d03be563e62bd7
cdwyer-cognito <chris.dwyer@cognitoiq.com>
2019-02-07 15:48:40 +0000

    restructured api for admin tasks

--- routes/admin.js
+++ routes/admin.js
@@ -22,70 +22,70 @@ function guid() {
 }
 
 router.post('/', async function( req, res ) {
-  const task = req.body;
+  const task = req.body.task;
+
   const authToken = req.headers['x-auth-token'];
 
-  if ( task.reloadDatabase ) {
-
-    if ( _authTokens.includes( authToken ) ) {
-      console.log( "Reloading database.xml file to songs table");
-      loadXML.loadXML()
-        .then( res.status(200).send() )
-        .catch(err => console.log( err ));
-    } else {
-      console.log( "not authorised to reload the songs table");
-      res.status(401).send( { error: "Not Authorised" } );
-    }
-  }
-
-  if ( task.clearRequests ) {
-
-    if ( _authTokens.includes( authToken ) ) {
-      console.log( "dropping the requests table");
-      await queryRequestsCollection.clearRequestsCollection()
-        .then( res.status(200).send() )
-        .catch(err => console.log( err ));
+  switch ( task ) {
+    case "login" : 
+      console.log( "Authentication request received!" );
+      if ( queryAdminCollection.authorised.includes( req.body.value ) ) {
+        console.log( "Authentication successful" );
+        const token = guid();
+        _authTokens.push(token); 
+        res.status(200).send( { token: token  } );
+      } else {
+        console.log( "Authentication failed" );
+        res.status(401).send( { error: "Not Authorised" } );
+      }
+      break;
+    case "logout":
+      console.log( "Logout requested" );
+      _authTokens = _authTokens.filter( token => token !== authToken ? token : null );
+      console.log( "Deleted auth token:", authToken );
+      res.status(200).send( { message: "logout successful" });
+      break;
+    case "reloadDatabase":
+      if ( _authTokens.includes( authToken ) ) {
+        console.log( "Reloading database.xml file to songs table");
+        loadXML.loadXML()
+          .then( res.status(200).send() )
+          .catch(err => console.log( err ));
+      } else {
+        console.log( "not authorised to reload the songs table");
+        res.status(401).send( { error: "Not Authorised" } );
+      }
+      break;
+    case "clearRequests":
+      if ( _authTokens.includes( authToken ) ) {
+        console.log( "dropping the requests table");
+        await queryRequestsCollection.clearRequestsCollection()
+          .then( res.status(200).send() )
+          .catch(err => console.log( err ));
       } else {
         console.log( "not authorised to drop the requests table");
         res.status(401).send( { error: "Not Authorised" } );
       }
-  }
+      break;
+    case "loadAdmin":
+      const dbs = paths.virtualDJdatabaseXMLFilepaths.map( filepath => {
+        return {
+          filepath: filepath,
+          connected: fs.existsSync( filepath )
+        }
+      })
 
-  if ( task.login ) {
-    console.log( "Authentication request received!" );
-    if ( queryAdminCollection.authorised.includes( task.value ) ) {
-      console.log( "Authentication successful" );
-      const token = guid();
-      _authTokens.push(token); 
-      res.status(200).send( { token: token  } );
-    } else {
-      console.log( "Authentication failed" );
-      res.status(401).send( { error: "Not Authorised" } );
-    }
-  }
-
-  if ( task.logout ) {
-    console.log( "Logout requested" );
-    _authTokens = _authTokens.filter( token => token !== authToken ? token : null );
-    console.log( "Deleted auth token:", authToken );
-    res.status(200).send( { message: "logout successful" });
-  }
-
-  if ( task.loadAdmin ) {
-    const dbs = paths.virtualDJdatabaseXMLFilepaths.map( filepath => {
-      return {
-        filepath: filepath,
-        connected: fs.existsSync( filepath )
+      const body = {
+        vdjDbPaths: dbs,
+        songCount: await loadXML.getSongCount()
       }
-    })
 
-    const body = {
-      vdjDbPaths: dbs,
-      songCount: await loadXML.getSongCount()
-    }
-
-    res.status(200).send( JSON.stringify( body ) );
+      res.status(200).send( JSON.stringify( body ) );
+      break;
+    default:
+      res.status(400).send( { error: "No matching admin request"});
   }
+
 });
 
 module.exports = router;
