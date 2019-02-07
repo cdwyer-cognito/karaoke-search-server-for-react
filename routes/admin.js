ab7d805834c53a361bd7ea8953d93ce82ec9e958
cdwyer-cognito <chris.dwyer@cognitoiq.com>
2019-02-07 14:32:43 +0000

    added auth token, will change at some point

--- routes/admin.js
+++ routes/admin.js
@@ -9,28 +9,54 @@ const queryAdminCollection = new QueryAdminCollection();
 const paths = require('../paths');
 const fs = require('fs');
 
+const _authTokens = [];
+
+function guid() {
+  function s4() {
+  return Math.floor( ( 1 + Math.random() ) * 0x10000)
+  .toString(16)
+  .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+  s4() + '-' + s4() + s4() + s4();
+}
+
 router.post('/', async function( req, res ) {
   const task = req.body;
 
   if ( task.reloadDatabase ) {
-    console.log( "Reloading database.xml file to songs table");
-    loadXML.loadXML()
-      .then( res.status(200).send() )
-      .catch(err => console.log( err ));
+
+    if ( _authTokens.includes( task.token ) ) {
+      console.log( "Reloading database.xml file to songs table");
+      loadXML.loadXML()
+        .then( res.status(200).send() )
+        .catch(err => console.log( err ));
+    } else {
+      console.log( "not authorised to reload the songs table");
+      res.status(401).send("{error: \"Not Authorised\"}");
+    }
   }
 
   if ( task.clearRequests ) {
-    console.log( "dropping the requests table");
-    await queryRequestsCollection.clearRequestsCollection()
-      .then( res.status(200).send() )
-      .catch(err => console.log( err ));
+
+    if ( _authTokens.includes( task.token ) ) {
+      console.log( "dropping the requests table");
+      await queryRequestsCollection.clearRequestsCollection()
+        .then( res.status(200).send() )
+        .catch(err => console.log( err ));
+      } else {
+        console.log( "not authorised to drop the requests table");
+        res.status(401).send("{error: \"Not Authorised\"}");
+      }
   }
 
   if ( task.login ) {
     console.log( "Authentication request received!" );
     if ( queryAdminCollection.authorised.includes( task.value ) ) {
       console.log( "Authentication successful" );
-      res.status(200).send();
+      const token = guid();
+      _authTokens.push(token); 
+      res.status(200).send( "{ token: " + token + "}");
     } else {
       console.log( "Authentication failed" );
       res.status(401).send("{error: \"Not Authorised\"}");
