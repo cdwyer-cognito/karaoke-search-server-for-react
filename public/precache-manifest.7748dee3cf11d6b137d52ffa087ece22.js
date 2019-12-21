2856ae545b79dc5eb34459eafbbb7c70bb6cca5f
cdwyer-cognito <chris.dwyer@cognitoiq.com>
2019-12-21 21:32:30 +0000

    updated build

---
+++ public/precache-manifest.7748dee3cf11d6b137d52ffa087ece22.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffe67121b17a16542e523a47fb00c323",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b0a931cb267f17e4d47",
+    "url": "/static/css/main.557b9d17.chunk.css"
+  },
+  {
+    "revision": "c501fb2a4289cfcb08bb",
+    "url": "/static/js/2.8cc350f2.chunk.js"
+  },
+  {
+    "revision": "a1210533514644c7f162",
+    "url": "/static/js/3.b31c6219.chunk.js"
+  },
+  {
+    "revision": "8ccf6c98c66b6d474b14",
+    "url": "/static/js/4.9d8807f2.chunk.js"
+  },
+  {
+    "revision": "4b0a931cb267f17e4d47",
+    "url": "/static/js/main.555d173a.chunk.js"
+  },
+  {
+    "revision": "0a8f21a2fe70bf32a702",
+    "url": "/static/js/runtime-main.4774b500.js"
+  },
+  {
+    "revision": "3a41409ad9c9deaee9617c8677c308e0",
+    "url": "/static/media/logo-karaoke-png.3a41409a.png"
+  },
+  {
+    "revision": "7af10c51719df79ddc73518fbac8fa2e",
+    "url": "/static/media/singers.7af10c51.png"
+  }
+]);
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.affec1b9148ce6d75cb45c7df1cc3377.js"
+  "/precache-manifest.7748dee3cf11d6b137d52ffa087ece22.js"
 );
 
 self.addEventListener('message', (event) => {
